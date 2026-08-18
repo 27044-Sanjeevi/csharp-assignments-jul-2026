7b38c2ef531218c94516ac1509569e09d41af8c2
Sanjeevi Senivasan <sanjeevi.senivasan@solitontech.com>
2026-08-18 09:36:17 +0530

    feat: add divide method

--- src/Assignment8ErrorHandling/Program.cs
+++ src/Assignment8ErrorHandling/Program.cs
@@ -1,11 +1,52 @@
-﻿namespace Assignments
+﻿using System.Runtime.CompilerServices;
+
+namespace Assignments
 {
+    /// <summary>
+    /// Provides methods for performing division operations and printing exception details.
+    /// </summary>
     internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main entry
+        /// </summary>
+        public static void Main()
         {
+            try
+            {
+                double res = Divide(10, 0);
+                Console.WriteLine(res);
+            }
+            catch (DivideByZeroException ex)
+            {
+                PrintException(ex);
+            }
 
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Divides the given two numbers.
+        /// </summary>
+        /// <param name="a">Dividend value</param>
+        /// <param name="b">Divisor Value</param>
+        /// <returns>Divided Value</returns>
+        public static double Divide(int a, int b)
+        {
+                return a / b;
+        }
+
+        /// <summary>
+        /// Prints exception
+        /// </summary>
+        /// <param name="ex">Exception object</param>
+        public static void PrintException(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+            Console.WriteLine(ex.Source);
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.TargetSite);
         }
     }
 }
