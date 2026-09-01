876d018684f8ba379adb02f4e5c64838387e9b3c
Sanjeevi Senivasan <sanjeevi.senivasan@solitontech.com>
2026-09-01 15:28:59 +0530

    feat: add empty md file for theory answers

---
+++ src/Assignment10UnderstandingDotnet/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment10UnderstandingDotnet
+{
+    internal class MathUtils
+    {
+        public int Add(int number1, int number2) => number1 + number2;
+
+        public int Subtract(int number1, int number2) => number1 - number2;
+
+        public int Multiply(int number1, int number2) => number1 * number2;
+
+        public int Divide(int number1, int number2) => number1 / number2;
+    }
+}
---
+++ src/Assignment10UnderstandingDotnet/TheoryQuestionsAndAnswers.md
@@ -0,0 +1,5 @@
+﻿# Exploration Questions
+
+>### 1. Explain what the .NET platform is and its primary purpose. 
+
+.NET Platform is primarily 
