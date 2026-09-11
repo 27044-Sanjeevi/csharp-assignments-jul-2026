382ae41a49e8c02143ee9e7fde47225d6775a089
Sanjeevi Senivasan <sanjeevi.senivasan@solitontech.com>
2026-09-11 15:05:42 +0530

    fix: nit improvements

--- src/Assignment13CollectionsAndGenerics/Collections/EnumerableAndReadonly.cs
+++ src/Assignment13CollectionsAndGenerics/Collections/EnumerableAndReadonly.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assignment13CollectionsAndGenerics.Data;
+﻿using Assignment13CollectionsAndGenerics.Data;
 
 namespace Assignment13CollectionsAndGenerics.Collections
 {
---
+++ src/Assignment13CollectionsAndGenerics/Collections/GenericsTask.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment13CollectionsAndGenerics
+{
+    /// <summary>
+    /// Represents task 5 demonstrating the use of generic collections.
+    /// </summary>
+    internal class GenericsTask
+    {
+        /// <summary>
+        /// Displays the generic collection implementation details.
+        /// </summary>
+        public void PerformOperations()
+        {
+            ConsoleHelpers.DisplayTitle("TASK 5 : APPLYING GENERICS");
+            ConsoleHelpers.DisplayStatus("Task 1 to Task 4 originally used concrete collection types.");
+            Console.WriteLine("Previous implementation : Commit 32519cb");
+            Console.WriteLine("\nCollections refactored to use generic types:\n");
+            Console.WriteLine("List<string> to List<T>");
+            Console.WriteLine("Stack<char> to Stack<T>");
+            Console.WriteLine("Queue<string> to Queue<T>");
+            Console.WriteLine("Dictionary<string, int> to Dictionary<TKey, TValue>");
+
+            Console.WriteLine();
+
+            ConsoleHelpers.DisplayStatus(
+                "Generics improve code reusability and type safety by allowing the same collection implementation " +
+                "to work with different data types.");
+        }
+    }
+}
