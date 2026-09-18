9b5294725d596551b02c6ecc1ae5c578abb6bf88
Sanjeevi Senivasan <sanjeevi.senivasan@solitontech.com>
2026-09-18 19:24:28 +0530

    feat: implement task 2

--- src/Assignment16AdvancedCSharpConcepts/Task2/VarKeywordDemo.cs
+++ src/Assignment16AdvancedCSharpConcepts/Task2/VarKeywordDemo.cs
@@ -11,25 +11,25 @@ namespace Assignment16AdvancedCSharpConcepts.Task2
         public void RunDemo()
         {
             ConsoleHelpers.DisplaySubtitle("Var Keyword Demo");
-            ConsoleHelpers.DisplayStatus("Initializing a new varible with var keyword..");
+            ConsoleHelpers.DisplayStatus("1. Initializing a new varible with var keyword..");
             ConsoleHelpers.PrintCodeSnippet("var stringValue = \"String value\";");
             var stringValue = "String value";
 
-            ConsoleHelpers.DisplayStatus("Trying to assign a different type value to the varible..");
+            ConsoleHelpers.DisplayStatus("2. Trying to assign a different type value to the variable..");
 
             // stringValue = 10; Compiler Error CS0029: Cannot implicitly convert type 'type' to 'type'
             ConsoleHelpers.PrintCodeSnippet("stringValue = 10;");
             ConsoleHelpers.DisplayFailure("This causes a Compiler Error CS0029: Cannot implicitly convert type 'type' to 'type'");
 
-            ConsoleHelpers.DisplayStatus("Trying to create a varible with var keyword with no initialization...");
+            ConsoleHelpers.DisplayStatus("3. Trying to create a varible with var keyword with no initialization...");
 
-            //var value;
+            // var value;
             ConsoleHelpers.PrintCodeSnippet("var value;");
             ConsoleHelpers.DisplayFailure("This causes a Compiler Error CS0818: Implicitly typed locals must be initialized");
 
-            ConsoleHelpers.DisplayStatus("Trying to assign null to a variable with var keyword...");
+            ConsoleHelpers.DisplayStatus("4. Trying to assign null to a variable with var keyword...");
 
-            //var value = null;
+            // var value = null;
             ConsoleHelpers.PrintCodeSnippet("var value = null;");
             ConsoleHelpers.DisplayFailure("This causes a Compiler Error: CS0815: Cannot assign 'expression' to an implicitly-typed variable.");
         }
