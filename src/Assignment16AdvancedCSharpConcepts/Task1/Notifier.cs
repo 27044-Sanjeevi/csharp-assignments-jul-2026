63133f2b3ccec235e217c3df2dddc0a942d6fc90
Sanjeevi Senivasan <sanjeevi.senivasan@solitontech.com>
2026-09-18 11:17:49 +0530

    feat: implement task 1

---
+++ src/Assignment16AdvancedCSharpConcepts/Task1/Notifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16AdvancedCSharpConcepts.Task1
+{
+    internal class Notifier
+    {
+        public delegate void Notify(string message);
+
+        public event Notify OnAction;
+
+        public void SendNotification(string message)
+        {
+            ConsoleHelpers.DisplayStatus($"Sending Notification to the publishers...");
+            this.OnAction?.Invoke(message);
+        }
+    }
+}
